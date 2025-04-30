4ffd05fd223a72ea3d4f2896803d4b59549ce1d8
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-04-30 18:12:41 -0300

    O decorator @Param é usado para ler valores dos parâmetros de rotas

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
@@ -10,7 +10,7 @@ export class RecadosController {
 
   // encontra um recado
   @Get(':id')
-  findOne() {
-    return 'Essa rota retorna apenas um recado';
+  findOne(@Param('id') id: string) {
+    return `Essa rota retorna apenas um recado: ${id}`;
   }
 }
