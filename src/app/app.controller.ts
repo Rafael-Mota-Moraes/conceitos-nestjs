8534ee65254345c4da5c0635729624d22a1ddfd7
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-04-30 11:13:39 -0300

    Service / Provider - entendendo o básico da injeção de dependências do NestJS

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
-// import { AppService } from './app.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  // constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return '<h1>Qualquer coisa 1</h1>';
+  }
+
+  @Get('exemplo')
+  exemplo() {
+    return this.appService.solucionaExemplo();
   }
 }
