04f2d85845875f13cf791caf6bbbd43033921c68
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-05-09 09:38:07 -0300

    Criando um módulo (Resource) para Entity "Pessoa"

--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -12,18 +12,6 @@ export class RecadosService {
     private readonly recadoRepository: Repository<Recado>,
   ) {}
 
-  private lastId = 1;
-  private recados: Recado[] = [
-    {
-      id: 1,
-      texto: 'Este é um recado de teste',
-      de: 'Joana',
-      para: 'João',
-      lido: false,
-      data: new Date(),
-    },
-  ];
-
   throwNotFoundError() {
     throw new NotFoundException('Recado não encontrado');
   }
