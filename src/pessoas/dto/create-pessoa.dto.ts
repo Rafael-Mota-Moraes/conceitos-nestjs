a691282b6351425249de6aab859ac5203140a9ef
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-05-10 19:42:51 -0300

    CRUD de Pessoa: criando o create com e-mail único na tabela (unique)

--- src/pessoas/dto/create-pessoa.dto.ts
+++ src/pessoas/dto/create-pessoa.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePessoaDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePessoaDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
+  nome: string;
+}
