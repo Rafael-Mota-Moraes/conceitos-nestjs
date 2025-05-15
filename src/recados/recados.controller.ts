b0ff8734852128d0069e5eae05cbbcc23a66334b
Rafael Mota Moraes <rafaelmota622@gmail.com>
2025-05-15 13:27:13 -0300

    DTO e validação de dados para paginação (take/limit e skip/offset do TypeOrm)

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -14,6 +14,7 @@ import {
 import { RecadosService } from './recados.service';
 import { CreateRecadoDto } from './dto/create-recado.dto';
 import { UpdateRecadoDto } from './dto/update-recado.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('recados')
 export class RecadosController {
@@ -23,10 +24,10 @@ export class RecadosController {
 
   @HttpCode(HttpStatus.OK)
   @Get()
-  findAll(@Query() pagination: any) {
-    const { limit = 10, offset = 10 } = pagination;
+  findAll(@Query() paginationDto: PaginationDto) {
+    const { limit = 10, offset = 10 } = paginationDto;
     // return `Essa rota retorna todos os recados: limit:${limit} offset:${offset}`;
-    return this.recadosService.findAll();
+    return this.recadosService.findAll(paginationDto);
   }
 
   // encontra um recado
--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -5,6 +5,7 @@ import { UpdateRecadoDto } from './dto/update-recado.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PessoasService } from 'src/pessoas/pessoas.service';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class RecadosService {
@@ -18,8 +19,12 @@ export class RecadosService {
     throw new NotFoundException('Recado não encontrado');
   }
 
-  async findAll() {
+  async findAll(paginationDto?: PaginationDto) {
+    const { limit = 10, offset = 10 } = paginationDto ?? {};
+
     const recados = await this.recadoRepository.find({
+      take: limit,
+      skip: offset,
       relations: ['de', 'para'],
       order: { id: 'desc' },
       select: {
